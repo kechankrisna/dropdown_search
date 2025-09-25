e7c7138e44627eb00da12e969513e95a58eaed68
kechankrisna <ke.chankrisna168@gmail.com>
2025-09-25 20:15:01 +0700

    chore: add support flutter 3.22.0

--- lib/dropdown_search.dart
+++ lib/dropdown_search.dart
@@ -324,7 +324,7 @@ class DropdownSearchState<T> extends State<DropdownSearch<T>> {
             Flexible(
               child: Text(
                 _selectedItemAsString(item),
-                style: Theme.of(context).textTheme.subtitle2,
+                style: Theme.of(context).textTheme.titleSmall,
                 overflow: TextOverflow.ellipsis,
               ),
             ),
@@ -365,7 +365,7 @@ class DropdownSearchState<T> extends State<DropdownSearch<T>> {
         );
       }
       return Text(_selectedItemAsString(getSelectedItem),
-          style: Theme.of(context).textTheme.subtitle1);
+          style: Theme.of(context).textTheme.titleMedium);
     }
 
     return selectedItemWidget();
